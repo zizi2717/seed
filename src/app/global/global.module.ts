9189aaf1647c2b901ba0a379ff1fb45c6640d198
JungDaeMan(ChoiMinSung) <zizi2717@belivvr.com>
2024-10-28 19:35:41 +0900

    Add event module

--- src/app/global/global.module.ts
+++ src/app/global/global.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 import { CacheModule } from './cache.module'
 import { DatabaseModule } from './database.module'
+import { EventModule } from './event.module'
 import { LoggerModule } from './logger.module'
 
 @Module({
-    imports: [DatabaseModule, CacheModule, LoggerModule]
+    imports: [DatabaseModule, CacheModule, LoggerModule, EventModule]
 })
 export class GlobalModule {}
